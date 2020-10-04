73c7130bbe1ebd2e41d51fab24310e156e9de297
mansong1 <martin.ansong@gmail.com>
2020-10-04 13:15:54 +0100

    Complete VM module

--- terraform/modules/vm/input.tf
+++ terraform/modules/vm/input.tf
@@ -1,1 +1,11 @@
+# Resource Group/Location
+variable "location" {}
+variable "resource_group" {}
 
+variable "resource_type" {}
+variable "application_type" {}
+variable "subnet_id" {}
+variable "publicip_id" {}
+variable "size" {}
+variable "admin_username" {}
+variable "network_interface_ids" {}
--- terraform/modules/vm/vm.tf
+++ terraform/modules/vm/vm.tf
@@ -1,25 +1,25 @@
-resource "azurerm_network_interface" "" {
-  name                = ""
-  location            = ""
-  resource_group_name = ""
+resource "azurerm_network_interface" "test" {
+  name                = "${var.application_type}-${var.resource_type}-nic"
+  location            = "${var.location}"
+  resource_group_name = "${var.resource_group}"
 
   ip_configuration {
     name                          = "internal"
-    subnet_id                     = ""
+    subnet_id                     = "${var.subnet_id}"
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = ""
+    public_ip_address_id          = "${var.publicip}"
   }
 }
 
-resource "azurerm_linux_virtual_machine" "" {
-  name                = ""
-  location            = ""
-  resource_group_name = ""
-  size                = ""
-  admin_username      = ""
-  network_interface_ids = []
+resource "azurerm_linux_virtual_machine" "test" {
+  name                = "${var.application_type}-${var.resource_type}-vm"
+  location            = "${var.location}"
+  resource_group_name = "${var.resource_group}"
+  size                = "${var.size}"
+  admin_username      = "${var.admin_username}"
+  network_interface_ids = [azurerm_network_interface.test.id]
   admin_ssh_key {
-    username   = ""
+    username   = "${var.admin_username}"
     public_key = "file("~/.ssh/id_rsa.pub")"
   }
   os_disk {
